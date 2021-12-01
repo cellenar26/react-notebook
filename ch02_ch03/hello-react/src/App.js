1b38c1006ec43557b9bcdc6f7609d15a53a50a9b
cellenar26 <cellenar26@gmail.com>
2021-12-01 20:15:01 +0900

    ch06 done

--- ch02_ch03/hello-react/src/App.js
+++ ch02_ch03/hello-react/src/App.js
@@ -7,14 +7,14 @@ import EventPractice from './EventPractice';
 import ValidationSample from './ValidationSample';
 import ScrollBox from './ScrollBox';
 import { Component } from 'react';
+import IterationSample from './IterationSample';
 
 class App extends Component {
 
   render() {
     return (
       <div>
-        <ScrollBox ref={ref => this.scrollBox = ref} />
-        <button onClick={() => this.scrollBox.scrollToBottom()}>to bottom</button>
+        <IterationSample />
       </div>
 
 
