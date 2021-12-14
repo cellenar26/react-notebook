382452d7fd85a6222188739cbb269913199b7d00
cellenar26 <cellenar26@gmail.com>
2021-12-14 21:03:26 +0900

    ~13.3

--- ch13/src/App.js
+++ ch13/src/App.js
@@ -1,15 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
-import { Route, Router, Routes, Switch } from 'react-router-dom';
+import { Link, Route, Router, Routes, Switch } from 'react-router-dom';
 import Home from './Home';
 import About from './About';
 
 function App() {
   return (
     <div className="App">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+      </ul>
+      <hr />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/"  element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/info' element={<About />} />
       </Routes>
     </div>
   );
