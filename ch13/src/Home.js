69d9fc21e479ee90c92bc74629999ae059b29588
cellenar26 <cellenar26@gmail.com>
2021-12-14 15:56:58 +0900

    router basic code

---
+++ ch13/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Home = () => {
+    return (
+        <div>
+            <h1>Home</h1>
+            <p>Home, the first looking page</p>
+        </div>
+    )
+}
+
+export default Home
